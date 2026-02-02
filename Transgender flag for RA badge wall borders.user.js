1c8aa08029c8859364def67c8b037abe0241697b
Veronica <61025394+VeronicaVyxie@users.noreply.github.com>
2026-02-02 16:25:25 +0100

    Update Transgender flag for RA badge wall borders.user.js

--- Transgender flag for RA badge wall borders.user.js
+++ Transgender flag for RA badge wall borders.user.js
@@ -1,13 +1,11 @@
 // ==UserScript==
 // @name         Transgender flag for RA badge wall borders
 // @namespace    RetroAchievements
-// @version      1.0.0
+// @version      1.0.1
 // @description  🏳️‍⚧️
 // @author       VeronicaVyxie
 // @match        https://retroachievements.org/user/*
-// @exclude      https://retroachievements.org/user/*/progress
 // @exclude      https://retroachievements.org/user/*/posts
-// @exclude      https://retroachievements.org/user/*/games
 // @exclude      https://retroachievements.org/user/*/developer/*
 // @exclude      https://retroachievements.org/user/*/tickets
 // @run-at       document-start
